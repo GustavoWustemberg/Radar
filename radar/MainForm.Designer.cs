d5d13123f511a090dd8259be98dfae58df2a17d7
Gusta <gustavowustemberg14@gmail.com>
2022-06-04 20:19:51 -0300

    Create Radar

---
+++ radar/MainForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mateus Moreira
+ * Date: 27/09/2021
+ * Time: 20:37
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace radar
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(12, 246);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(79, 28);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Posição:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(111, 246);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(47, 30);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(606, 246);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(95, 27);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Posição:";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(725, 246);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(47, 30);
+			this.textBox2.TabIndex = 3;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox3.Location = new System.Drawing.Point(391, 244);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(47, 30);
+			this.textBox3.TabIndex = 4;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(235, 246);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(150, 28);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Tempo Gasto:";
+			this.label3.Click += new System.EventHandler(this.Label3Click);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(316, 365);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(132, 41);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Caulcular";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 21);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(798, 219);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 7;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label4
+			// 
+			this.label4.BackColor = System.Drawing.Color.Aqua;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(41, 54);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(77, 23);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Radar 1";
+			// 
+			// label5
+			// 
+			this.label5.BackColor = System.Drawing.Color.Aqua;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.Location = new System.Drawing.Point(695, 54);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(77, 23);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Radar 2";
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(262, 295);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(255, 23);
+			this.label6.TabIndex = 12;
+			// 
+			// label7
+			// 
+			this.label7.Location = new System.Drawing.Point(262, 318);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(255, 44);
+			this.label7.TabIndex = 13;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Location = new System.Drawing.Point(547, 295);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(240, 186);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 14;
+			this.pictureBox2.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.ClientSize = new System.Drawing.Size(822, 517);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.Name = "MainForm";
+			this.Text = "radar";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ radar/MainForm.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mateus Moreira
+ * Date: 27/09/2021
+ * Time: 20:37
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace radar
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();	
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			float p1, p2, t, vm;
+			p1 = float.Parse(textBox1.Text);
+			p2 = float.Parse(textBox2.Text);
+			t = float.Parse(textBox3.Text);
+			vm = (p2 - p1)/t;
+			label6.Text = ("A Sua velocidade foi: " + vm + " Km/h").ToString();
+				
+			if(vm <= 80){
+			
+				label7.Text = ("Você está dentro do limite " +
+				               "de velocidade.").ToString();
+				pictureBox2.Load("poze.jpg");
+				
+				
+			} else{
+			
+				label7.Text = ("Você está acima do limite" +
+			               	   "de velocidade.").ToString();
+				pictureBox2.Load("irmão.jpg");
+			}
+		}
+		void Label3Click(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
